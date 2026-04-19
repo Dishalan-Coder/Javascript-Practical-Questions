ab7302dad14453e9887ab84cb4cd5e2de4ccfdd2
Dishalan-Coder <dishalandisha0103@gmail.com>
2026-04-20 00:10:35 +0530

    day4

---
+++ Day4.js
@@ -0,0 +1,24 @@
+// Exercises: Level 1
+
+// 1.
+function fullName() {
+  console.log("Jeyachchanthiran Dishalan");
+}
+
+// call the function
+fullName();
+
+// 2.
+function fullName1(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+
+// calling the function
+let fuName = fullName1("Jeyachchanthiran", "Dishalan");
+console.log(fuName);
+
+// 3.
+
+
+
+
